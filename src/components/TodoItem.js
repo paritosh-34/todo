aef8db6fe68a48e53b34e78acc9a8c4df28b8d75
Paritosh Batish <batishparitosh2@gmail.com>
2020-05-28 10:07:22 +0530

    Time Bug Solved:Time showing Different in different browsers

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,8 +13,15 @@ class TodoItem extends Component {
             {this.props.todo.date.toLocaleDateString()}
           </div>
           <div className="todo-time">
-            {this.props.todo.time.from.toLocaleTimeString().slice(0, 4)} -{" "}
-            {this.props.todo.time.to.toLocaleTimeString().slice(0, 4)}
+            {this.props.todo.time.from.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}{" "}
+            -{" "}
+            {this.props.todo.time.to.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
           </div>
         </div>
       </div>
