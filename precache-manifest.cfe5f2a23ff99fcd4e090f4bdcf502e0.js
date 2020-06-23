360e7c2538cd75bb495cf8d27f8c9b20f0c92a32
Paritosh Batish <batishparitosh2@gmail.com>
2020-06-23 19:59:21 +0530

    Updates

---
+++ precache-manifest.cfe5f2a23ff99fcd4e090f4bdcf502e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec12c9e33480ec84c4aeedf38196c363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44bbe4b15e7112f89983",
+    "url": "/static/css/main.8e462014.chunk.css"
+  },
+  {
+    "revision": "26dee9d3bb9a42a76eca",
+    "url": "/static/js/2.00c2e2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00c2e2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bbe4b15e7112f89983",
+    "url": "/static/js/main.c9f83d88.chunk.js"
+  },
+  {
+    "revision": "37a0d35451170c581fd7",
+    "url": "/static/js/runtime-main.c1487c63.js"
+  }
+]);
